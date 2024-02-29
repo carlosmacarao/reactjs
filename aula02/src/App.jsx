e6574d0b9fa702bd7f2008b3254fe7b12f15387e
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2024-02-29 10:42:47 +0200

    new update

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -2,12 +2,7 @@ import Button from "./Button";
 
 function App () {
 
-    let tab = 0
-    let n = 10
-
-    for (let c = 0; c <= n; c++) {
-        tab = n * c
-    }
+    
 
     return (
         <div>
