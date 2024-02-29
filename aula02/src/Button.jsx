d8b195d940d2f01e808851f74155c6bbdeb4f292
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2024-02-29 10:06:57 +0200

    initial commit

---
+++ aula02/src/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Button.css';
+
+function Button () {
+
+    return (
+        <>
+            <button>Confirm</button>
+        </>
+    )
+}
+
+export default Button;
--- aula02/src/main.jsx
+++ aula02/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Button from './Button';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+   <>
+        <App />
+        
+   </>
 )
